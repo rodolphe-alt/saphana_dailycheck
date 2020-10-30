bfdfeb60814cd3c5cf017c2ca8cfdd384535f29e
Rodolphe ALT <73311580+rodolphe-alt@users.noreply.github.com>
2020-10-30 09:38:50 +0100

    Update 02-dailycheck_saphana_initos.cmd

--- 02-dailycheck_saphana_initos.cmd
+++ 02-dailycheck_saphana_initos.cmd
@@ -4,3 +4,5 @@ pip install StringIO
 pip install bitmath
 pip install PrettyTable
 mkdir -p /opt/scripts/moni_hana/
+mkdir /opt/scripts/moni_hana/1969700
+# download SQL standard query from 1969700 and unzip in this folder /opt/scripts/moni_hana/1969700
